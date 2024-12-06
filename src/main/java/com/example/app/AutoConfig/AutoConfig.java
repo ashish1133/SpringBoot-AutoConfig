1967a0b17e6f427a71fbec7efd09d35083842eb7
priya <ashishashok893@gmail.com>
2024-12-07 02:32:52 +0530

    AutoConfiger User Deatils

--- src/main/java/com/example/app/AutoConfig/AutoConfig.java
+++ src/main/java/com/example/app/AutoConfig/AutoConfig.java
@@ -1,20 +1,50 @@
 package com.example.app.AutoConfig;
 
+import org.apache.coyote.Request;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+import java.security.Security;
 
-public class AutoConfig {
+class AutoConfig{
 
-    //AutoConfig
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity Http)throws Exception{
+
+        Http.csrf(Customizer->Customizer.disable());
+        Http.authorizeHttpRequests(Request->Request.anyRequest().authenticated());
+        Http.formLogin(Customizer.withDefaults());
+        Http.httpBasic(Customizer.withDefaults());
+        Http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return Http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("ashok")
+                .password("ashok")
+                .roles("user")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("smitha")
+                .password("sm")
+                .roles("House WIfe")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
 
 }
